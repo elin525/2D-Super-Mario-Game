6c02979b40c7b2a5510de287733a00110b04d486
Alex Lopez <alopez31148@csu.fullerton.edu>
2025-02-25 15:08:19 -0800

    Added question blocks, still need editing

---
+++ scripts/question_area.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+var activate = true
+var coordinate: Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body: Node2D) -> void:
+	if activate == true:
+		activate = false
+		$Sprite2D.frame = 1
+		#position.y -= 10
+		#var timer = get_tree().create_timer(0.1)
+		#await timer.timeout
+		#position = coordinate
