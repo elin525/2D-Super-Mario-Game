84cefe54bf87dca74c277e6f0251044d80f4e0f4
Nathaniel Obeso <nathaniel.obeso@csu.fullerton.edu>
2025-02-24 21:56:37 -0800

    Removed extra blocks from map since the camera is now added to stop at the flagpole, also re-added features accidentally deleted during commit

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,19 +1,1 @@
 extends TileMap
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	for i in range(446, 50000):
-		if i % 2 == 0:
-			self.set_cell(0, Vector2i(i, 0), 0, Vector2i(0, 0), 0)
-			self.set_cell(0, Vector2i(i, 1), 0, Vector2i(0, 1), 0)
-			self.set_cell(0, Vector2i(i, 2), 0, Vector2i(0, 0), 0)
-			self.set_cell(0, Vector2i(i, 3), 0, Vector2i(0, 1), 0)
-		else:
-			self.set_cell(0, Vector2i(i,0), 0, Vector2i(1,0), 0)
-			self.set_cell(0, Vector2i(i,1), 0, Vector2i(1,1), 0)
-			self.set_cell(0, Vector2i(i,2), 0, Vector2i(1,0), 0)
-			self.set_cell(0, Vector2i(i,3), 0, Vector2i(1,1), 0)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
