93ac94e19c37079d537e121eedc9bb7b18776ae3
Alex Lopez <alopez31148@csu.fullerton.edu>
2025-02-26 17:20:29 -0800

    Slight movement for the block

--- scripts/question_area.gd
+++ scripts/question_area.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 var activate = true
-var coordinate: Vector2
+#var coordinate: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,7 +15,8 @@ func _on_body_entered(body: Node2D) -> void:
 	if activate == true:
 		activate = false
 		$Sprite2D.frame = 1
-		#position.y -= 10
-		#var timer = get_tree().create_timer(0.1)
-		#await timer.timeout
+		position.y -= 10
+		var timer = get_tree().create_timer(0.2)
+		await timer.timeout
+		position.y += 10
 		#position = coordinate
