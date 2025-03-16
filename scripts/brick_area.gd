d76eb1fe5aaf1d4a8259738a5111ffa9b4f6d0c6
Alex Lopez <alopez31148@csu.fullerton.edu>
2025-03-15 21:20:34 -0700

    Updated bricks with animations to break

--- scripts/brick_area.gd
+++ scripts/brick_area.gd
@@ -1,13 +1,27 @@
 extends Area2D
 
+@onready var player = get_node("../TileMap/player")
+@onready var brick = $Sprite2D
+
+var piece_sprites = []
+
 func _ready() -> void:
-	pass
+	piece_sprites.append($Piece_Animation1)
+	piece_sprites.append($Piece_Animation2)
+	piece_sprites.append($Piece_Animation3)
+	piece_sprites.append($Piece_Animation4)
+	
+	for sprite in piece_sprites:
+		sprite.visible = false
 
 func _process(delta: float) -> void:
 	pass
 
 func _on_body_entered(body: Node2D) -> void:
-	shift_block()
+	if player.current_state == "small":
+		shift_block()
+	elif player.current_state == "big" or player.current_state == "fire":
+		delete_block()
 
 func shift_block():
 	var sound = get_node("../Animation Sounds")
@@ -15,16 +29,46 @@ func shift_block():
 	sound.playing = true
 	var temp_timer = get_tree().create_timer(0.05)
 	await temp_timer.timeout
-	
-	position.y -= 10
+	brick.position.y -= 10
 	var timer = get_tree().create_timer(0.2)
 	await timer.timeout
-	position.y += 10
+	brick.position.y += 10
 
 func delete_block():
+	# Play Sounds
 	var sound = get_node("../Animation Sounds")
 	sound.stream = load("res://sounds/blockbreak.wav")
 	sound.playing = true
+	
+	# Setup
+	for sprite in piece_sprites:
+		sprite.visible = true
+		sprite.play()
+	
+	brick.visible = not brick.visible
+	for i in range(10):
+		var temp_timer = get_tree().create_timer(0.01)
+		for j in range(len(piece_sprites)):
+			piece_sprites[j].position.y -= 2
+			if j < 2:
+				piece_sprites[j].position.x -= 2
+			else:
+				piece_sprites[j].position.x += 2
+		await temp_timer.timeout
+	
+	for i in range(20):
+		var temp_timer = get_tree().create_timer(0.01)
+		for j in range(len(piece_sprites)):
+			piece_sprites[j].position.y += 6
+			if j < 2:
+				piece_sprites[j].position.x -= 1
+			else:
+				piece_sprites[j].position.x += 1
+		await temp_timer.timeout
+	
+	# Disable visibility, animation, and hitboxes
 	$CollisionShape2D.set_deferred("disabled", true)
-	$Sprite2D.visible = not $Sprite2D.visible
 	$StaticBody2D/CollisionShape2D.set_deferred("disabled", true)
+	for piece in piece_sprites:
+		piece.visible = false
+		piece.stop()
