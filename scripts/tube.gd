ff295a2c2c628c26060a3ffdf779fc3d401a9b69
Alex Lopez <alopez31148@csu.fullerton.edu>
2025-04-10 17:45:05 -0700

    Minor terminal clear

--- scripts/tube.gd
+++ scripts/tube.gd
@@ -27,10 +27,8 @@ func _process(delta: float) -> void:
 				player.is_controllable = true
 
 func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	print("Entered")
 	accessable = true
 
 
 func _on_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	print("Exited")
 	accessable = false
